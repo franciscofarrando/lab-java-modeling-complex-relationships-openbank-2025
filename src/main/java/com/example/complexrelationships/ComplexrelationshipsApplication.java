612fdfcd65fbaaeb41e0fe84b2b377e688e926be
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-04-26 12:38:04 +0200

    init

---
+++ src/main/java/com/example/complexrelationships/ComplexrelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.example.complexrelationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexrelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexrelationshipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/complexrelationships/ComplexrelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.complexrelationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplexrelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
